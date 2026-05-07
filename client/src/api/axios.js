b8e4cec0a7f9755dca70bad446cef1ef3fcc5e80
sameer-ssr99 <sameersr999.sr@gmail.com>
2026-05-07 22:43:03 +0530

    Update API baseURL for production deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: import.meta.env.PROD 
+    ? 'https://advisorpath-server.onrender.com/api' 
+    : '/api'
 });
 
 let logoutCallback = null;
